e4f639feb500acc99b2959978be6eaf158539295
hooman naghiee <hooman@RTLer.com>
2016-10-03 15:30:26 +0330

    change travis

--- compiler.sh
+++ compiler.sh
@@ -1,4 +1,19 @@
+npm install -g aglio
 rm index.apib
 cat documents/header.apib >> index.apib
 cat documents/pages/*.apib >> index.apib
 aglio -i index.apib -o index.html
+
+git config --global user.email "zarinpal@Travis"
+git config --global user.name "zarinpal@Travis"
+git config --global push.default simple
+eval "$(ssh-agent -s)" #start the ssh agent
+chmod 600 .travis_key # this key should have push access
+ssh-add .travis_key
+git clone git@github.com:ZarinPal-Lab/API-Docs.git
+git checkout --orphan gh-pages
+cp -rv index.html API-Docs
+cd API-Docs
+git add --all
+git commit -m "Travis CI"
+git push
